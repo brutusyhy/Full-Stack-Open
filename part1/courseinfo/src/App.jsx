84f574b68bd85d1d53600c45b845666d3f54c0e9
Hongyu Yang <brutusyhy@gmail.com>
2024-10-12 13:49:06 +0300

    Part1. Courseinfo

---
+++ part1/courseinfo/src/App.jsx
@@ -0,0 +1,34 @@
+import Header from './Header';
+import Content from './Content';
+import Total from "./Total";
+
+const App = () => {
+    const course = {
+        name: 'Desenvolvimento de aplicação Half Stack',
+        parts: [
+            {
+                name: 'Fundamentos da biblioteca React',
+                exercises: 10
+            },
+            {
+                name: 'Usando props para passar dados',
+                exercises: 7
+            },
+            {
+                name: 'Estado de um componente',
+                exercises: 14
+            }
+        ]
+    }
+    return (
+        <div>
+            <Header course={course.name}/>
+            <Content parts={course.parts}/>
+            <Total parts={course.parts}/>
+        </div>
+    )
+}
+
+
+
+export default App
